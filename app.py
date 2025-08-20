106744ca8fc268b2bbfbdc62727c3402627ced8a
Ubuntu <ubuntu@ip-172-31-41-124.eu-west-2.compute.internal>
2025-08-20 07:25:36 +0000

    Code files added

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host="db",
+        user="root",
+        password="rootpassword",
+        database="testdb"
+    )
+
+@app.route('/')
+def home():
+    return "Flask + MySQL with Docker Compose 🚀"
+
+@app.route('/users')
+def get_users():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users;")
+    users = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(users)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
